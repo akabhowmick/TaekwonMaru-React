f210627f60cc044819ce2ce733f7c8ca36f06df9
akabhowmick <47453997+akabhowmick@users.noreply.github.com>
2024-03-14 00:24:01 -0400

    update home and contact

--- src/Components/Pages/Contact/ContactForm.tsx
+++ src/Components/Pages/Contact/ContactForm.tsx
@@ -3,103 +3,111 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 import emailjs from "@emailjs/browser";
 
+const serviceId = "service_x1v468p";
+const templateId = "template_8przsfw";
+const publicKey = "aI0R8QkNkegFHRxBK";
+
 export const ContactForm = () => {
   const [buttonState, setButtonState] = useState("Send Message");
+
   const formik = useFormik({
     initialValues: {
       from_name: "", //user name
-      subject: "", // subject of email
+      phone_number: "", // phoneNumber of user
       reply_to: "", // user email
       message: "", // message of email
+      class_of_interest: "", //class that user wants to ask about
     },
     validationSchema: Yup.object({
       from_name: Yup.string().required("* Name field is required"),
-      subject: Yup.string().required("* Subject field is required"),
+      phone_number: Yup.string()
+        .required("* Phone Number is required")
+        .matches(/[0-9]{10}/, "Enter your 10 digit phone number with no spaces")
+        .max(10, "Only enter 10 digits"),
       reply_to: Yup.string().email("Invalid email address").required("* Email field is required"),
       message: Yup.string().required("* Message field is required"),
+      class_of_interest: Yup.string().required(
+        "* Picking a class wil help us give you the most relevant information"
+      ),
     }),
     onSubmit: (values, { setSubmitting, resetForm }) => {
+      setButtonState("Sending Email");
       try {
-        emailjs
-          .send("service_iodyf8e", "template_s9bux1j", values, "a8dh5CLKMF5OX1aLy")
-          .then(() => {
-            setButtonState("Send Email");
-            setSubmitting(false);
-            resetForm();
-          });
+        emailjs.send(serviceId, templateId, values, publicKey).then(() => {
+          setButtonState("Send Email");
+          setSubmitting(false);
+          resetForm();
+        });
       } catch {
         setButtonState("Send Email");
         setSubmitting(false);
       }
     },
   });
+
+  const contactFormInput = [
+    { name: "Name:", label: "from_name" },
+    { name: "Email", label: "reply_to" },
+    { name: "Phone Number", label: "phone_number" },
+    { name: "Class Of Interest", label: "class_of_interest" },
+    { name: "Message", label: "message" },
+  ];
+
+  // allows the input to have access to correct formik values
+  const getFormikValues = (label: string) => {
+    if (label === "from_name") {
+      return formik.values.from_name;
+    } else if (label === "reply_to") {
+      return formik.values.reply_to;
+    } else if (label === "phone_number") {
+      return formik.values.phone_number;
+    } else if (label === "message") {
+      return formik.values.message;
+    } else if (label === "class_of_interest") {
+      return formik.values.class_of_interest;
+    }
+    return "";
+  };
+
+  // allows access to the error message in formik
+  const getFormikErrors = (label: string) => {
+    if (label === "from_name") {
+      return formik.errors.from_name;
+    } else if (label === "reply_to") {
+      return formik.errors.reply_to;
+    } else if (label === "phone_number") {
+      return formik.errors.phone_number;
+    } else if (label === "message") {
+      return formik.errors.message;
+    } else if (label === "class_of_interest") {
+      return formik.errors.class_of_interest;
+    }
+    return "";
+  };
+
   return (
     <form className="formcontact" onSubmit={formik.handleSubmit}>
       <div className="form-row-1">
-        <div className="contact-form-div">
-          <label htmlFor="from_name">Name:</label>
-          <input
-            className="contact-form-input"
-            id="from_name"
-            name="from_name"
-            type="text"
-            autoComplete="off"
-            placeholder="YOUR NAME"
-            onChange={formik.handleChange}
-            value={formik.values.from_name}
-          />
-          <div className={`expandable ${formik.errors.from_name ? "show" : ""}`}>
-            {formik.errors.from_name}
-          </div>
-        </div>
-        <div className="contact-form-div">
-          <label htmlFor="subject">Subject</label>
-          <input
-            className="contact-form-input"
-            id="subject"
-            name="subject"
-            type="text"
-            autoComplete="off"
-            placeholder="SUBJECT"
-            onChange={formik.handleChange}
-            value={formik.values.subject}
-          />
-          <div className={`expandable ${formik.errors.subject ? "show" : ""}`}>
-            {formik.errors.subject}
-          </div>
-        </div>
-        <div className="contact-form-div">
-          <label htmlFor="reply_to">Email</label>
-          <input
-            className="contact-form-input"
-            id="reply_to"
-            type="email"
-            name="reply_to"
-            placeholder="YOUR EMAIL"
-            autoComplete="off"
-            onChange={formik.handleChange}
-            value={formik.values.reply_to}
-          />
-          <div className={`expandable ${formik.errors.reply_to ? "show" : ""}`}>
-            {formik.errors.reply_to}
-          </div>
-        </div>
-
-        <div className="contact-message-div">
-          <label htmlFor="message">Message</label>
-          <textarea
-            className="contact-form-input"
-            id="message"
-            name="message"
-            placeholder="YOUR MESSAGE"
-            autoComplete="off"
-            onChange={formik.handleChange}
-            value={formik.values.message}
-          />
-          <div className={`expandable ${formik.errors.message ? "show" : ""}`}>
-            {formik.errors.message}
-          </div>
-        </div>
+        {contactFormInput.map(({ name, label }) => {
+          return (
+            <div key={name} className="contact-form-div">
+              <label htmlFor={label}>{name}</label>
+              <input
+                className="contact-form-input"
+                id={label}
+                name={label}
+                type="text"
+                autoComplete="off"
+                placeholder={`Your ${name}`}
+                onChange={formik.handleChange}
+                value={getFormikValues(label)}
+              />
+              {formik.submitCount > 0 && getFormikErrors(label) && (
+                <div className="expandable show">{getFormikErrors(label)}</div>
+              )}
+            </div>
+          );
+        })}
         <div className="col-12">
           <button
             id="contact-submit-btn"
