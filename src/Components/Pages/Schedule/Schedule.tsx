af10af13f06beba37189742ad4121d774806e66a
Akash <akabhowmick@gmail.com>
2024-09-05 14:59:26 -0400

    update kpop 9/2024

--- src/Components/Pages/Schedule/Schedule.tsx
+++ src/Components/Pages/Schedule/Schedule.tsx
@@ -9,7 +9,7 @@ const eventsInfo = [
     title: "KPOP - Dance:",
     details: [
       "Friday 6:45-7:45pm (09/06 ~ 09/27)",
-      "NCT 127 ",
+      "NCT 127 - Walk",
       "Saturday 1:30pm-2:20pm (08/31 ~ 09/21)",
       "TXT - I will see you tomorrow",
     ],
