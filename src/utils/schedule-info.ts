3044330db7fe4afc46cafd77ba7551dd101f40ca
Akash <akabhowmick@gmail.com>
2024-11-21 15:30:26 -0500

    update 2024 nov/dec kpop

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,10 +6,10 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45-7:45pm (11/1 ~ 11/22)",
-        "Bad Villain - +82",
-        "Saturday 1:30pm-2:20pm (10/26 ~ 11/16)",
-        "Straykids - Chk Chk Boom",
+        "Friday 6:45-7:45pm (12/6 ~ 12/27)",
+        "Enhypen - No Doubt ",
+        "Saturday 1:30pm-2:20pm (11/23, 12/7 ~ 12/21)",
+        "Riize - Boom Boom Base ",
         "",
       ],
     },
