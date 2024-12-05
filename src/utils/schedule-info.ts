d0c40b55e54a2401deaa45029a1603c4bb3fb2a2
Akash <akabhowmick@gmail.com>
2024-12-05 15:23:51 -0500

    update dec 2024 events

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,9 +6,9 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45-7:45pm (12/6 ~ 12/27)",
+        "Friday 6:45-7:45pm (12/06,13,20 & 01/03)",
         "Enhypen - No Doubt ",
-        "Saturday 1:30pm-2:20pm (11/23, 12/7 ~ 12/21)",
+        "Saturday 1:30pm-2:20pm (11/25, 12/07,14,21)",
         "Riize - Boom Boom Base ",
         "",
       ],
@@ -16,19 +16,19 @@ export const schedule_info = {
     {
       title: currentMonth + " Events:",
       details: [
-        "11/17 - NYS Championship",
-        "11/28 - 30 No Classes (Thanksgiving Break)",
-        "",
-        "",
-        "",
+        "12/10 - Black Belt Test",
+        "12/21 - Little Tiger Test",
+        "12/23 - Regular Test",
+        "12/24, 25 - School Closed (Christmas Break)",
+        "12/31, 1/1 - School Closed (New Year)",
       ],
     },
     {
       title: "Junior Leader Meeting:",
       details: [
-        "11/26/24 07:20pm - 07:55pm",
+        "12/26/24 07:20pm - 07:55pm",
         "Mandatory Monthly Meeting for Junior Leaders!",
-        "",
+        "End of year party!",
         "",
         "",
       ],
