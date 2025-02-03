da07f96f80333550a2d6749824b4970986beade2
Akash <akabhowmick@gmail.com>
2025-02-03 15:16:26 -0500

    update 2025 feb

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,7 +6,7 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45-7:45pm (01/10, 17, 24, 31)",
+        "Friday 6:45-7:45pm (01/10, 17, 24, 02/07)",
         "Ateez - Ice on my Teeth ",
         "Saturday 1:30pm-2:20pm (01/04, 11, 18, 25)",
         "G-Dragon - Home Sweet Home ",
@@ -16,8 +16,8 @@ export const schedule_info = {
     {
       title: currentMonth + " Events:",
       details: [
-        "01/20 - No class (MLK Day)",
-        "01/30 - Family class",
+        "02/10 - 15 Buddy Week",
+        "02/14 - Valentine's Day Special Class",
         "",
         ""
       ],
@@ -25,7 +25,7 @@ export const schedule_info = {
     {
       title: "Junior Leader Meeting:",
       details: [
-        "01/28/24 07:20pm - 07:55pm",
+        "02/25/25 07:20pm - 07:55pm",
         "Mandatory Monthly Meeting for Junior Leaders!",
         "",
         "",
