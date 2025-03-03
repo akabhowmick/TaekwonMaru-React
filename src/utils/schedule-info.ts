6f38a96d64615d7bf74998ae5578e468d4df0b62
Akash <akabhowmick@gmail.com>
2025-03-03 15:21:35 -0500

    update 2025 march schedule events

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,25 +6,25 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45-7:45pm (02/14, 21, 28, 03/07)",
-        "Seventeen - LOVE, MONEY, FAME ",
-        "Saturday 1:30pm-2:20pm (02/01, 08, 15, 22)",
-        "Stray Kids - Walking on Water",
+        "Friday 6:35-7:50pm (3/07,14,21)",
+        "G-Dragon - Power",
+        "Saturday 1:30pm-2:35pm (3/01,08,25)",
+        "NCT Dream - When I'm with you ",
       ],
     },
     {
       title: currentMonth + " Events:",
       details: [
-        "02/10 - 15 Buddy Week",
-        "02/14 - Valentine's Day Special Class",
-        "02/17 - No Class (President's Day)",
-        "03/22 - Championship"
+        "03/03 - Promotion Test",
+        "03/22 - Hong Ik Championship",
+        "",
+        ""
       ],
     },
     {
       title: "Junior Leader Meeting:",
       details: [
-        "02/25/25 07:20pm - 07:55pm",
+        "03/25/25 07:20pm - 07:55pm",
         "Mandatory Monthly Meeting for Junior Leaders!",
         "",
         "",
