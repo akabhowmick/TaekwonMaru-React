7873c1917ec9d95706b55fd1f00cffbe4cb0520b
Akash <akabhowmick@gmail.com>
2025-10-09 15:12:09 -0400

    add oct 2025 events

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -9,26 +9,21 @@ export const schedule_info = {
         "Friday 6:45 - 7:45pm (9/26 & 10/10, 17, 24)",
         "RIIZE - Fly up ",
         "Saturday 1:30pm - 2:20pm (9/27 & 10/11, 19, 25)",
-        "Straykids - Ceremony"
+        "Straykids - Ceremony",
       ],
     },
     {
       title: currentMonth + " Events:",
       details: [
-        "09/02 - 07: Buddy Week",
-        "09/11 - Long Islands Duck Performance",
-        "",
+        "10/13 - No Classes (Indigenous People's Day)",
+        "10/27 - Promotion Test",
+        "10/22 & 10/29 - Schedule Change Due to Championship Practice",
         "",
       ],
     },
     {
       title: "Junior Leader Meeting:",
-      details: [
-        "TBD",
-        "Mandatory Monthly Meeting for Junior Leaders!",
-        "",
-        "",
-      ],
+      details: ["TBD", "Mandatory Monthly Meeting for Junior Leaders!", "", ""],
     },
   ],
   scheduleTitle: `Upcoming Events For: ${currentMonth}`,
