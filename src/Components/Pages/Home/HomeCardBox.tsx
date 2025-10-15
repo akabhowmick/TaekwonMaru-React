e261ca6d2e8dc640f8c25d840bee33d22271db41
Akash <akabhowmick@gmail.com>
2025-10-15 11:22:41 -0400

    update home box

--- src/Components/Pages/Home/HomeCardBox.tsx
+++ src/Components/Pages/Home/HomeCardBox.tsx
@@ -1,29 +1,37 @@
 import { HomeCardInfo } from "../../../types/interfaces";
 import "./Home.css";
 
-export const HomeCardBox = ({ cardInfo }: { cardInfo: HomeCardInfo}) => {
+export const HomeCardBox = ({ cardInfo }: { cardInfo: HomeCardInfo }) => {
   const { title, src, description, type } = cardInfo;
 
   const mediaToDisplay =
     type === "image" ? (
-      <img src={src} alt={`image for {title}`} loading="lazy" />
+      <div className="media-wrapper">
+        <img src={src} alt={`image for ${title}`} loading="lazy" />
+      </div>
     ) : (
-      <iframe
-        src={src}
-        title="YouTube video player"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
-        allowFullScreen
-      ></iframe>
+      <div className="media-wrapper">
+        <iframe
+          src={src}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
+          allowFullScreen
+        ></iframe>
+      </div>
     );
 
   return (
-    <div className="p-4 flex flex-col">
+    <div className="p-4 flex flex-col h-full">
       {mediaToDisplay}
-      <h3 className="subheader text-2xl underline">{title}</h3>
-      <div className="description">{description}</div>
-      <a href="/contact" className="btn-primary">
-        Book a Class
-      </a>
+      <div className="p-6 flex flex-col flex-grow justify-between">
+        <div>
+          <h3 className="subheader text-2xl font-bold mb-3">{title}</h3>
+          <div className="description mb-4">{description}</div>
+        </div>
+        <a href="/contact" className="btn-primary no-underline inline-block mt-auto">
+          Book a Class
+        </a>
+      </div>
     </div>
   );
 };
