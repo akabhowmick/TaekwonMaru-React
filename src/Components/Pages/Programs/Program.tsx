c0d7d7c9f11ed0237023b8a5b9695d72c29e1288
Akash <akabhowmick@gmail.com>
2025-10-17 23:07:31 -0400

    style:update programs

--- src/Components/Pages/Programs/Program.tsx
+++ src/Components/Pages/Programs/Program.tsx
@@ -6,7 +6,7 @@ export const Program = () => {
     <section>
       <h3 className="section-header title-capitalize">Programs:</h3>
       <div className="portfolio-section">
-        <div className="portfolio-grid">
+        <div className="portfolio-grid grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
           {classList.map((program) => {
             return (
               <div key={program.title} className="portfolio-card">
