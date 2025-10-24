0921e70475c135c9455fe63d0140e7a320068652
Akash <akabhowmick@gmail.com>
2025-10-24 11:49:57 -0400

    update2025 nov events

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,18 +6,18 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45 - 7:45pm (9/26 & 10/10, 17, 24)",
-        "RIIZE - Fly up ",
-        "Saturday 1:30pm - 2:20pm (9/27 & 10/11, 19, 25)",
-        "Straykids - Ceremony",
+        "Friday 6:45 - 7:45pm (10/31, 11/7,14,21)",
+        "Cortis - Fashion ",
+        "Saturday 1:30pm - 2:20pm (11/1,8,15,22)",
+        "All day project - Famous",
       ],
     },
     {
       title: currentMonth + " Events:",
       details: [
-        "10/13 - No Classes (Indigenous People's Day)",
-        "10/27 - Promotion Test",
-        "10/22 & 10/29 - Schedule Change Due to Championship Practice",
+        "11/16 - NYS Governor's Championship",
+        "",
+        "",
         "",
       ],
     },
