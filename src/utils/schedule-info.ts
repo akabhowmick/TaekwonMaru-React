83aeae633fbd841ee436083857b47eb91bddd777
Akash Bhowmick - Personal <akabhowmick@gmail.com>
2025-12-04 10:22:35 -0500

    update dec kpop 2025 events

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,10 +6,10 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45 - 7:45pm (10/31, 11/7,14,21)",
-        "Cortis - Fashion ",
-        "Saturday 1:30pm - 2:20pm (11/1,8,15,22)",
-        "All day project - Famous",
+        "Friday 6:45 - 7:45 PM (12/5, 12, 19 & 1/2)",
+        "All day project - One more time",
+        "Saturday 1:30 PM - 2:20 PM (12/6, 13, 30 & 1/3)",
+        "Stray Kids - Famous",
       ],
     },
     {
