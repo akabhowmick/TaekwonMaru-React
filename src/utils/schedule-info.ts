94be95d38e39ca9583b023e330f981ff2e30ebb3
Akash Bhowmick - Personal <akabhowmick@gmail.com>
2026-01-21 21:08:36 -0500

    update kpop 2026 jan

--- src/utils/schedule-info.ts
+++ src/utils/schedule-info.ts
@@ -6,10 +6,10 @@ export const schedule_info = {
     {
       title: "KPOP - Dance Class:",
       details: [
-        "Friday 6:45 - 7:45 PM (12/5, 12, 19 & 1/2)",
-        "All day project - One more time",
-        "Saturday 1:30 PM - 2:20 PM (12/6, 13, 30 & 1/3)",
-        "Stray Kids - Famous",
+        "Friday 6:45 - 7:45 PM",
+        "Jennie/with the IE & Seventeen/Worth it",
+        "Saturday 1:30 PM - 2:20 PM",
+        "Boynextdoor/Hollywood action ",
       ],
     },
     {
